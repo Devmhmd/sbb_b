c03b204f6692863e19ac28c499829d715d3d9ea9
Devmhmd <104868036+Devmhmd@users.noreply.github.com>
2022-06-04 09:43:08 +0200

    Add files via upload

--- sbb_b/plugins/الكشف.py
+++ sbb_b/plugins/الكشف.py
@@ -56,13 +56,13 @@ async def fetch_info(replied_user, event):
     user_bio = "⌔∮ هذا المستخدم ليس لديه اي نبذة" if not user_bio else user_bio
     rozrtba = (
         ".「  مطـور السورس  」."
-        if user_id == 1694386561 or user_id == 2034443585 or user_id == 1715051616
+        if user_id == 5227347612 or user_id == 2034443585 or user_id == 1715051616
         else (".「  العضـو  」.")
     )
     rozrtba = (
         ".「 مـالك الحساب  」."
         if user_id == (await event.client.get_me()).id
-        and user_id != 1694386561
+        and user_id != 5227347612
         and user_id != 2034443585
         and user_id != 1715051616
         else rozrtba
